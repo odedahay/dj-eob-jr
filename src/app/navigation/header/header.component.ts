78e2006704d31fa64d05f0996d54d63a08796b4d
odedahay <odedahay@yahoo.com>
2021-03-10 14:18:56 +0800

    Set up website for JR

---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
+  }
+
+  onToggleSidenav(){
+    this.sidenavToggle.emit();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();  
+  }
+
+}
---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.scss']
+})
+export class SidenavListComponent implements OnInit, OnDestroy {
+  @Output() closeSidenav = new EventEmitter<void>();
+  isAuth: boolean = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus =>{
+      this.isAuth = authStatus;
+    })
+  }
+
+  onClose(){
+    this.closeSidenav.emit();
+  }
+
+  ngOnDestroy(){
+    this.authSubscription.unsubscribe();
+  }
+
+}
